e8116b0f8c109bd8408afe064d12507013625574
PD1ce <padeisinger@gmail.com>
2014-03-09 13:45:49 -0500

    more stuff

---
+++ pickMyDrink/pickMyDrink/Data.h
@@ -0,0 +1,13 @@
+//
+//  Data.h
+//  pickMyDrink
+//
+//  Created by Philip Deisinger on 3/9/14.
+//  Copyright (c) 2014 Philip Deisinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Data : NSObject
+
+@end
---
+++ pickMyDrink/pickMyDrink/Data.m
@@ -0,0 +1,13 @@
+//
+//  Data.m
+//  pickMyDrink
+//
+//  Created by Philip Deisinger on 3/9/14.
+//  Copyright (c) 2014 Philip Deisinger. All rights reserved.
+//
+
+#import "Data.h"
+
+@implementation Data
+
+@end
